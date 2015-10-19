9dc56a7ce09654f9665d5d7078f2a315f6d8e4af
boxsome <jan.castor14@gmail.com>
2015-10-19 17:34:56 -0400

    Fixed positioning of tooltips for icons.

--- src/js/project-icons.js
+++ src/js/project-icons.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $(".project__meta__heading__icons__item").hover(function() {
+  $(".project-icon").hover(function() {
     $(this).children(".project-icon-tooltip").toggleClass("project-icon-tooltip--active");
   }, function() {
     $(this).children(".project-icon-tooltip").toggleClass("project-icon-tooltip--active");
