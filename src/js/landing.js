5bcc1f4e822d612875e4589f77f7572caaba0818
boxsome <jan.castor14@gmail.com>
2015-10-20 17:13:02 -0400

    Added responsiveness for landing page content.

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-  $(".landing__row__project__overlay").hover(function() {
-    var $overlay = $(this).siblings(".landing__row__project__img");
+  $(".landing__project__overlay").hover(function() {
+    var $overlay = $(this).siblings(".landing__project__img");
     $overlay.toggleClass("hovered");
     $(this).toggleClass("hovered");
   }, function() {
     $(this).toggleClass("hovered");
-    $(this).siblings(".landing__row__project__img").toggleClass("hovered");
+    $(this).siblings(".landing__project__img").toggleClass("hovered");
   });
 
 });
