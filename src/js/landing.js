1e64fa22a95bb04c05bc9cd61ff6a1ce0d272ae4
boxsome <jan.castor14@gmail.com>
2015-10-21 15:05:06 -0400

    Added code to replace thumbnails on landing page with retina images where appropriate.

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
-  $(".landing__project__overlay").hover(function() {
-    var $overlay = $(this).siblings(".landing__project__img");
-    $overlay.toggleClass("hovered");
-    $(this).toggleClass("hovered");
-  }, function() {
-    $(this).toggleClass("hovered");
-    $(this).siblings(".landing__project__img").toggleClass("hovered");
-  });
+  var $landingPage = $("main.landing"),
+      $landingThumbnails;
 
+  if ($landingPage.length > 0) { //check if we're on the landing page
+
+    $(".landing__project__overlay").hover(function () {
+      var $overlay = $(this).siblings(".landing__project__img");
+      $overlay.toggleClass("hovered");
+      $(this).toggleClass("hovered");
+    }, function () {
+      $(this).toggleClass("hovered");
+      $(this).siblings(".landing__project__img").toggleClass("hovered");
+    });
+
+    //add retina for thumbnails
+    if (isHighDensity()) {
+      $landingThumbnails = $landingPage.find(".landing__project__img");
+      $landingThumbnails.css("background-image", $(this).css("background-image").replace("thumbnail", "thumbnail_2x"));
+    }
+  }
 });
