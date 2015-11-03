4d52d4b79fc6355c7c2e25115be0895a2d9898ce
boxsome <jan.castor14@gmail.com>
2015-11-03 16:50:48 -0500

    Added mobile menu button in header.

--- src/js/header-menu-mobile.js
+++ src/js/header-menu-mobile.js
@@ -2,16 +2,10 @@
 $(document).ready(function() {
 
   //full menu toggle on mobile
-  $("button.header__nav__mobile__menu").click(function() {
-    $(this).toggle(); //hide button
-    $(".header-menu-mobile").toggle(); //toggle full menu
-    $("body").css("overflow", "hidden"); //make the full screen non-scrollable
+  $(".header__mobile__nav__button, .menu-overlay").click(function() {
+    $(".header__mobile__nav").toggleClass("active"); //toggle full menu
+    $("body").toggleClass("unscrollable"); //make the full screen non-scrollable
+    $("main").toggleClass("menu-active");
   });
 
-  //full menu close on mobile
-  $(".header-menu-mobile__close").click(function(){
-    $(".header-menu-mobile").toggle(); //hide menu
-    $("button.header__nav__mobile__menu").toggle(); //show menu button again
-    $("body").css("overflow", ""); //make the body scrollable again
-  })
 });
